afe526c1667bd6e3c7ba13420e9ef495ad6eec01
MXuDong <2941884109@qq.com>
2018-10-24 08:58:49 +0800

    Log4j 整合

---
+++ src/test/java/TestLog4j.java
@@ -0,0 +1,13 @@
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+
+public class TestLog4j {
+
+    @Test
+    public void testLog4jProperties(){
+        Logger logger = LogManager.getLogger();
+        logger.error("Hello Log4J");
+    }
+}
