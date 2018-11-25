c26a82495eb8a39b1121507a44e5d6b6d67d3987
MXuDong <2941884109@qq.com>
2018-11-25 17:52:40 +0800

    修復完成

---
+++ src/test/java/TestMappers.java
@@ -0,0 +1,21 @@
+import dao.UserInfosMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+//配置文件的位置
+//若当前配置文件名=当前测试类名-context.xml 就可以在当前目录中查找@ContextConfiguration()
+@ContextConfiguration("classpath:applicationContext.xml")
+public class TestMappers {
+
+    @Autowired
+    UserInfosMapper userInfosMapper;
+
+    @Test
+    public void TestMapper(){
+        System.out.println(userInfosMapper.selectByPrimaryKey(0));
+    }
+}
